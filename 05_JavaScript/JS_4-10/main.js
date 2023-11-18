ccf2523d5361d60aa7cf69c786970979ae772c3f
regaliz-develop <sales.regaliz@gmail.com>
2023-11-18 13:41:42 +0900

    ファイル修正

---
+++ 05_JavaScript/JS_4-10/main.js
@@ -0,0 +1,14 @@
+// 【JS_4-10】下記 grade の連想配列から英語の点数だけの配列を作成し、点数を 2 倍にしてください。
+const grade = [
+  { name: "佐藤", subject: "math", score: 78 },
+  { name: "高橋", subject: "English", score: 84 },
+  { name: "鈴木", subject: "English", score: 90 },
+  { name: "田中", subject: "English", score: 62 },
+  { name: "山本", subject: "math", score: 54 },
+  { name: "加藤", subject: "math", score: 88 },
+  { name: "中村", subject: "English", score: 68 },
+];
+
+// 加工した結果をresultに格納
+// const result =
+console.log(result); //[168, 180, 124, 136]
---
+++ 05_JavaScript/JS_4-6/main.js
@@ -0,0 +1,47 @@
+// 【JS_4-6】 分割代入と map を用いて、users をコメントアウトのような形になるように加工し、result という変数に格納し、出力してください。
+const users = [
+  {
+    id: 1,
+    detail: {
+      name: 'Hoge',
+      age: '22',
+    }
+  },
+  {
+    id: 2,
+    detail: {
+      name: 'Fuga',
+      age: '25',
+    }
+  },
+  {
+    id: 3,
+    detail: {
+      name: 'Piyo',
+      age: '33',
+    }
+  }
+];
+
+// 加工した結果をresultに格納
+// const result =
+// console.log(result)
+
+// 出力結果
+// [
+//   {
+//     id: 1,
+//     name: 'Hoge',
+//     age: '22',
+//   },
+//   {
+//     id: 2,
+//     name: 'Fuga',
+//     age: '25',
+//   },
+//   {
+//     id: 3,
+//     name: 'Piyo',
+//     age: '33',
+//   },
+// ]
